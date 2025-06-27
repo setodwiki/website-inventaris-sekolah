0ebe80a6e14bfa7cbe09824d5d34a756375c79e1
setodwiki <setodwiki@gmail.com>
2025-06-27 15:30:15 +0700

    mengedit file sidebar.php

--- config/sidebar.php
+++ config/sidebar.php
@@ -6,7 +6,7 @@
         <!-- <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div> -->
-        <div class="sidebar-brand-text mx-3">INVENTARIS <sup>v1</sup></div>
+        <div class="sidebar-brand-text mx-3">INVENTARIS WEBSITE<sup>v1</sup></div>
     </a>
 
     <!-- Divider -->
